b188a6987bfacadc53aacb9c1d64cdd3e9ad7b67
ZEYNEP KÖSE <zeynep.j17@gmail.com>
2022-08-16 00:04:12 +0300

    upload

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -7,18 +7,44 @@
       <th>Order Name</th>
       <th>Date</th>
       <th>Total</th>
+      <th class="table is-striped is-narrow is-hoverable is-fullwidth">Details</th>
     </tr>
   </thead>
   <tbody>
-    <tr
+    <tr 
       v-for="orders in orders"
       :key="orders.id">
       <td>{{ orders.orderName }}</td>
       <td>{{ orders.orderDate }}</td>
       <td>{{ orders.total }}</td>
+      <td>
+      <div>
+          <table class="table is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>
+            <tr
+              v-for="details in orders.details"
+              :key="details.id">
+              <td>{{ details.product.description3 }}</td>
+              <td>{{ details.price }}</td>
+            </tr>
+          </td>
+        </tbody>
+        </table>
+        </div>
+        
+      </td>
     </tr>
   </tbody>
   </table>
+ 
+
   </div>
 </template>
 
